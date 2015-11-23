757a9cf9161781eaa998b2164468bc7d87973e70
Marcel Patzlaff <mpatzlaff@benocs.com>
2015-11-23 20:45:38 +0100

    CHANGE: evaluated further information about payee account

--- src/ofxstatement/plugins/germany_1822direkt.py
+++ src/ofxstatement/plugins/germany_1822direkt.py
@@ -1,47 +1,60 @@
 from ofxstatement.plugin import Plugin
 from ofxstatement.parser import CsvStatementParser
-from ofxstatement.statement import StatementLine
+from ofxstatement.statement import BankAccount, StatementLine
+
+TMAPPINGS = {
+    "20005": "CASH",
+    "20032": "PAYMENT",
+    "20080": "PAYMENT",
+    "20200": "DIRECTDEBIT",
+    "21050": "XFER",
+    "21094": "XFER",
+    "25100": "FEE"
+}
+
 
 class FrankfurterSparkasse1822Plugin(Plugin):
     def get_parser(self, filename):
         encoding = self.settings.get('charset', 'iso-8859-1')
         f = open(filename, 'r', encoding=encoding)
         parser = FrankfurterSparkasse1822Parser(f)
-        parser.statement.account_id = self.settings['account']
         parser.statement.bank_id = self.settings.get('bank', '50050201')
         parser.statement.currency = self.settings.get('currency', 'EUR')
         return parser
 
 
 class FrankfurterSparkasse1822Parser(CsvStatementParser):
-    """
-    This plugin tries to parse the provided CSV data into the same
-    format, as the discontinued OFX export
-    """
     date_format = "%d.%m.%Y"
 
     def parse_float(self, f):
-	# convert a number in german localization (e.g. 1.234,56) into a float
-        return float(f.replace('.','').replace(',','.'))
+        # convert a number in german localization (e.g. 1.234,56) into a float
+        return float(f.replace('.', '').replace(',', '.'))
 
     def parse_record(self, line):
         # FIXME: add header validation
         #print(self.cur_record, line)
         if self.cur_record < 2:
             return None
+
         if len(line) < 3:
             """e.g.: ['# 1 vorgemerkte Umsätze nicht angezeigt']"""
             return None
         if not line[2]:
             return None
+
+        if self.statement.account_id is None:
+            self.statement.account_id = line[0]
+
         sl = StatementLine()
         sl.id = line[1]
-        sl.date = self.parse_datetime(line[2])
+        sl.date = self.parse_datetime(line[3])  # using the valuta date here
         sl.amount = self.parse_float(line[4])
-        sl.trntype = 'DEBIT' if sl.amount < 0 else 'CREDIT'
+        sl.trntype = TMAPPINGS.get(line[5], 'DEBIT' if sl.amount < 0 else 'CREDIT')
         sl.payee = line[7]
-        sl.memo = "(%s/%s): %s"  % (line[8],line[9], " ".join(line[15:]).strip())
+        sl.memo = " ".join(x for x in line[15:33] if len(x) > 0)
+
+        if len(line[8]) > 0 and len(line[9]) > 0:
+            # additional bank information if present
+            sl.bank_account_to = BankAccount(line[9], line[8])
 
         return sl
-
-
